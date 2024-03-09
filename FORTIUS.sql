606343365e2f01cd352aa3011f9c548de63b5fe3
Karlyvelasquez <144060912+Karlyvelasquez@users.noreply.github.com>
2024-03-08 20:13:54 -0500

    Taller 2

---
+++ FORTIUS.sql
@@ -0,0 +1,18 @@
+
+ALTER TABLE Employee
+ADD CONSTRAINT FK_Department_Employee FOREIGN KEY (Dno) REFERENCES Department(Dnumber);
+
+
+ALTER TABLE Dept_Locations
+ADD CONSTRAINT FK_Department_Dept_Locations FOREIGN KEY (Dnumber) REFERENCES Department(Dnumber);
+
+ALTER TABLE Project
+ADD CONSTRAINT FK_Department_Project FOREIGN KEY (Dnum) REFERENCES Department(Dnumber);
+
+
+ALTER TABLE Works_On
+ADD CONSTRAINT FK_Employee_Works_On FOREIGN KEY (Essn) REFERENCES Employee(Ssn);
+
+
+ALTER TABLE Dependent
+ADD CONSTRAINT FK_Employee_Dependent FOREIGN KEY (Essn) REFERENCES Employee(Ssn);
